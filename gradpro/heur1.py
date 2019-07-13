29af39206d16aea149215a220d1289e68d9c7e06
Ccharliiee <wangs220@mcmaster.ca>
2019-07-12 22:59:35 -0400

    Add files via upload

--- gradpro/heur1.py
+++ gradpro/heur1.py
@@ -1,4 +1,10 @@
 import numpy as np
+import gradpro.six003deluxe as sd
 
-startv = np.loadtxt('perv_d2.csv', dtype=int)
-print(startv)
+startv = sd.picr(2)
+print('sv:', startv)
+
+for i,item in enumerate(startv):
+    startv[i] = np.insert(item, 0, 0, axis=i%2)
+
+print('svl:', startv)
