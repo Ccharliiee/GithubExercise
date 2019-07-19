06561ff522d9cb7dff74402f2d23c8afa7797679
Ccharliiee <wangs220@mcmaster.ca>
2019-07-19 15:50:34 -0400

    Add files via upload

--- gradpro/heur1funcs.py
+++ gradpro/heur1funcs.py
@@ -5,23 +5,19 @@ import numpy as np
 def conemembershipstemplate(point, coef1='', coef2='', const=0, coordsumthresh=dim * k / 2,
                             leqflag=True):  # if leq is in ineqs, leqflag=T
     coordsum = sum(point)
-    insider = True
     if coordsum < coordsumthresh and coef1 != '':  # if '', not in this case
         for i0 in point: #check apex 0
             if coordsum - i0 > coef1 * i0 and leqflag or coordsum - i0 < coef1 * i0 and not leqflag:  # handle leq or geq
-                insider = False
-                print('000 sk')
-                return insider
+                # print('000 sk')
+                return False
     elif coordsum > coordsumthresh and coef2 != '':
         for i0 in point: #check apex s(k)
             if coordsum - i0 > coef2 * i0 + const and leqflag or coordsum - i0 < coef2 * i0 + const and not leqflag:  # handle leq or geq
-                insider = False
-                print('111d sk')
-                return insider
+                return False
     else:
-        print(insider)
-        return insider
-    return insider
+        # print(insider)
+        return True
+    return True
 
 def conememberships(point):
     coordsum = sum(point)
@@ -48,18 +44,67 @@ def conememberships(point):
 
 
 # conememberships([3, 3, 3])
+
+#####################test1
 # print(conemembershipstemplate([1, 1, 1], 2 * dim - 2, (k - 2) * (dim - 1) / (k - 1), k * (dim - 1) / (k - 1), 2 * dim - 1))
-
+#
 # print(conemembershipstemplate([1, 2, 2], dim-2, dim, -k, (dim-1)*k/2, False))
-
+#
 # print(conemembershipstemplate([2.5, 2, 2], '', dim-1-(1/k), 1, 1))
-
+#
 # print(conemembershipstemplate([0.5, 1.5, 1.5],(1/(k/2 +1)) + dim-2, dim+ (2/(k-2)), (-(2*k)/(k-2)) -k, (k/2 +1)*(dim-1) +1, False))
-
+#
 # print(conemembershipstemplate([2, 1.5, 2],(dim-1 -(1/k)), '', 0, dim*k -1, False))
-
+#
 # print(conemembershipstemplate([4, 4, 4],(k-2)*(dim-1)/(k-1), 2*(dim-1), -k*(dim-1), dim*k-2*dim-1, False))
-
+#
 # print(conemembershipstemplate([1, 1, 2], dim, '', 0, dim*k/2 + k/2))
+#
+# print(conemembershipstemplate([2.5, 2.5, 3.5], (dim*k-2*dim+2)/(k-2) , (dim-2) + 2/(k+2),  k*(1 - 2/(k+2)), dim*k/2 + k/2 -dim))
+##################################test1/
 
-print(conemembershipstemplate([2.5, 2.5, 3.5], (dim*k-2*dim+2)/(k-2) , (dim-2) + 2/(k+2),  k*(1 - 2/(k+2)), dim*k/2 + k/2 -dim))
+
+#####################test2
+point = [1, 2, 3]
+#
+# print(conemembershipstemplate(point, 2 * dim - 2, (k - 2) * (dim - 1) / (k - 1), k * (dim - 1) / (k - 1), 2 * dim - 1))
+#
+# print(conemembershipstemplate(point, dim-2, dim, -k, (dim-1)*k/2, False))
+#
+# print(conemembershipstemplate(point, '', dim-1-(1/k), 1, 1))
+#
+# print(conemembershipstemplate(point,(1/(k/2 +1)) + dim-2, dim+ (2/(k-2)), (-(2*k)/(k-2)) -k, (k/2 +1)*(dim-1) +1, False))
+#
+# print(conemembershipstemplate(point,(dim-1 -(1/k)), '', 0, dim*k -1, False))
+#
+# print(conemembershipstemplate(point,(k-2)*(dim-1)/(k-1), 2*(dim-1), -k*(dim-1), dim*k-2*dim-1, False))
+#
+# print(conemembershipstemplate(point, dim, '', 0, dim*k/2 + k/2))
+#
+# print(conemembershipstemplate(point, (dim*k-2*dim+2)/(k-2) , (dim-2) + 2/(k+2),  k*(1 - 2/(k+2)), dim*k/2 + k/2 -dim))
+##################################test2/
+
+def runconemembstempl(point):
+    # if conemembershipstemplate(point, 2 * dim - 2, (k - 2) * (dim - 1) / (k - 1), k * (dim - 1) / (k - 1), 2 * dim - 1) or
+    # conemembershipstemplate(point, dim - 2, dim, -k, (dim - 1) * k / 2, False) or conemembershipstemplate(point, '', dim-1-(1/k), 1, 1) or
+    #     conemembershipstemplate(point, (1 / (k / 2 + 1)) + dim - 2, dim + (2 / (k - 2)), (-(2 * k) / (k - 2)) - k,
+    #                             (k / 2 + 1) * (dim - 1) + 1, False) or conemembershipstemplate(point,(dim-1 -(1/k)), '', 0, dim*k -1, False) or
+    #     conemembershipstemplate(point, (k - 2) * (dim - 1) / (k - 1), 2 * (dim - 1), -k * (dim - 1),
+    #                             dim * k - 2 * dim - 1, False) or conemembershipstemplate(point, dim, '', 0, dim*k/2 + k/2) or
+    #     conemembershipstemplate(point, (dim * k - 2 * dim + 2) / (k - 2), (dim - 2) + 2 / (k + 2),
+    #                             k * (1 - 2 / (k + 2)), dim * k / 2 + k / 2 - dim)
+    conemembs =[conemembershipstemplate(point, 2 * dim - 2, (k - 2) * (dim - 1) / (k - 1), k * (dim - 1) / (k - 1), 2 * dim - 1),
+                conemembershipstemplate(point, dim - 2, dim, -k, (dim - 1) * k / 2, False),
+                conemembershipstemplate(point, '', dim-1-(1/k), 1, 1),
+                conemembershipstemplate(point, (1 / (k / 2 + 1)) + dim - 2, dim + (2 / (k - 2)),(-(2 * k) / (k - 2)) - k, (k / 2 + 1) * (dim - 1) + 1, False),
+                conemembershipstemplate(point, (dim - 1 - (1 / k)), '', 0, dim * k - 1, False),
+                conemembershipstemplate(point, (k - 2) * (dim - 1) / (k - 1), 2 * (dim - 1), -k * (dim - 1), dim * k - 2 * dim - 1, False),
+                conemembershipstemplate(point, dim, '', 0, dim * k / 2 + k / 2),
+                conemembershipstemplate(point, (dim * k - 2 * dim + 2) / (k - 2), (dim - 2) + 2 / (k + 2), k * (1 - 2 / (k + 2)), dim * k / 2 + k / 2 - dim)
+                ]
+    for conememb in conemembs:
+        if conememb:
+            return True
+    return False
+
+print(runconemembstempl(point))
