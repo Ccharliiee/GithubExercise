12df3105e97b8a80e8007426387c8c3e0129b73a
Ccharliiee <wangs220@mcmaster.ca>
2019-07-31 16:19:17 -0400

    after some attempt to d=5
    after try to brute force for d=5 which didn't complete in 20h, I found that it is impossible to brute force d=5 to get vertices and its generators. I believe it is because it takes more time to store generators at each iteration. So now use the result of last year which only contains vertices d=5
    
    Add more function body in the verificationall() function to verify d=5 due to different file struture
    
    Monitored a bug, cannot use a d for all since the main func needs to import other funcs using d in other files

--- gradpro/heur1.py
+++ gradpro/heur1.py
@@ -6,6 +6,8 @@ from gradpro.heur1funcs import conemembasmb as cmb
 
 
 
+dim = 3
+k = 2 ** (dim - 1)
 
 ################### init vcand ver nonver
 def initial():
@@ -21,10 +23,23 @@ def initial():
     # vred = vred + vredsym
     # print('smallverlift:',vred, 'len', len(vred))
 
-    # vcand = np.append(vred, np.array([1,1,3])) 0….0, 0…01, 1…1d, 0k/2…k/2, k/2-1 … k/2-1 k-1
     vcand = list(vred)
-    vcand.append(np.array([1, 1, 1, 4]))
-    vcand.append(np.array([[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 0, 1], [1, 0, 0, 1]]))       # 1111d's generator
+    # vcand = np.append(vred, np.array([1,1,3]))#0….0, 0…01, 1…1d, 0k/2…k/2, k/2-1 … k/2-1 k-1
+    vcand.append(np.array([1, 1, 3]))
+    vcand.append(np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1]]))       # 1111d's generator
+
+    # vcand.append(np.array([1, 1, 1, 4]))
+    # vcand.append(np.array([[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 0, 1], [1, 0, 0, 1]]))       # 1111d's generator
+    # vcand.append(np.array([3, 3, 3, 7]))
+    # vcand.append(np.array([[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 0, 1], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1]]))  # k/2-1 … k/2-1 k-1's generator
+
+    # vcand.append(np.array([1, 1, 1, 1, 5]))
+    # vcand.append(np.array([[0, 0, 0, 0, 1], [0, 0, 0, 1, 1], [0, 0, 1, 0, 1], [0, 1, 0, 0, 1], [1, 0, 0, 0, 1]]))  # 1111d's generator
+    # vcand.append(np.array([1, 9, 9, 9, 9]))
+    # twokplus1 = [[1]*dim]
+    # for gen in itertools.product(range(2), repeat=dim-1):
+    #     twokplus1.append(np.insert(gen, 0, 0))
+    # vcand.append(np.array(twokplus1)) # k/2-1 … k/2-1 k-1's generator
     # print('vcandbasic:', vcand, 'len', len(vcand))
 
     # vertices = np.array(vcand)
@@ -36,7 +51,7 @@ def initial():
         nonvertices.append([1] * (dim - 1) + [i])
     # print('nonvertices:',nonvertices, 'len', len(nonvertices))
 
-    determinedpoints = np.array([[0] * dim, [0, 0, 0, 1], [0, 4, 4, 4]])
+    determinedpoints = np.array([[0] * dim, [0] * (dim-1)+[1], [0]+[k//2]*(dim-1)])
     vcanddict = {}
     for i in range(0, len(vcand), 2):
         vcandval = []
@@ -48,74 +63,113 @@ def initial():
         vcanddict.pop(' '.join(np.array2string(deterp, separator=',')))
     # print('Udetervcandval: ', vcanddict)
 
-    return vcanddict, vertices, nonvertices
-
+    vcandobs = []
+    for verger in vcanddict.values():
+        vcandob = sd.Point(verger[0], verger[1])
+        vcandobs.append(vcandob)
+    return vcandobs, vertices, nonvertices
+##########vcandobs is with vertex and generator
 
 ########################################
-dim = 4
-k = 2 ** (dim - 1)
-vcanddict, vertices, nonvertices = initial()
-print('vcanddict: ', vcanddict)
-print('vertices:', vertices, 'len', len(vertices))
-print('nonvertices:', nonvertices, 'len', len(nonvertices))
 
-#################################################hash vertex non vertex
-verticesdict={}
-nonverticesdict={}
-undeterpts={}
-for ver in vertices:
-    verticesdict[' '.join(np.array2string(np.array(ver), separator=','))] = ver
-for nonver in nonverticesdict:
-    nonverticesdict[' '.join(np.array2string(np.array(nonver), separator=','))] = nonver
+def runheu1():
+    vcandobs, vertices, nonvertices = initial()
+    print('vcandobs: ', vcandobs)
+    print('vertices:', vertices, 'len', len(vertices))
+    print('nonvertices:', nonvertices, 'len', len(nonvertices))
 
-print('verticesdict:', verticesdict, 'len', len(verticesdict))
-print('nonvertices:', nonverticesdict, 'len', len(nonverticesdict))
-###################################################
-edges = list(itertools.product(range(2), repeat=dim))
-edgesdict = {}
-for edge in np.array(edges[1:]):
-    edgesdict[' '.join(np.array2string(edge, separator=','))] = edge
-print('edges:', edgesdict)
-for key, val in vcanddict.items():
-    edgeavail = edgesdict.copy()
+    ################################################hash vertex non vertex
+    verticesdict={}
+    nonverticesdict={}
 
-    for gernerator in val[1]:
-        gerstr = ' '.join(np.array2string(np.array(gernerator), separator=','))
-        if gerstr in edgesdict.keys():
-            edgeavail.pop(gerstr)
-    print('point', val)
-    print('edgeavailable:', edgeavail.values())
+    for ver in vertices:
+        verticesdict[' '.join(np.array2string(np.array(ver), separator=','))] = ver
+    for nonver in nonverticesdict:
+        nonverticesdict[' '.join(np.array2string(np.array(nonver), separator=','))] = nonver
 
-    for forward in edgeavail.values():
-        newpoint = np.add(val[0], forward)
-        newpointstr = ' '.join(np.array2string(np.array(sorted(newpoint)), separator=','))
-        if newpointstr in verticesdict:
-            continue
-        elif newpointstr in nonverticesdict:
-            continue
-        elif newpointstr in undeterpts:
-            continue
-        elif sum(np.isin(newpoint, (0, k))):
-            continue
-        elif sum(newpoint) > dim*k/2:
-            continue
-        elif cmb(newpoint):
-            nonvertices.append(newpoint)
-            nonverticesdict[newpointstr] = newpoint
-            continue
-        else:
-            undeterpts[newpointstr] = newpoint
-print('undeterpts ', undeterpts.keys())
-vv = []
-vv.append(' '.join(np.array2string(np.array([1, 1, 4, 4]), separator=',')))
-vv.append(' '.join(np.array2string(np.array([1, 2, 2, 5]), separator=',')))
-vv.append(' '.join(np.array2string(np.array([1, 3, 5, 5] ), separator=',')))
-vv.append(' '.join(np.array2string(np.array([2, 2, 3, 6]), separator=',')))
-vv.append(' '.join(np.array2string(np.array([2, 2, 4, 6]), separator=',')))
-vv.append(' '.join(np.array2string(np.array([1, 2, 4, 5]), separator=',')))
-fg=True
-for v in vv:
-    if v not in undeterpts.keys():
-        fg=False
-        print('missed: ', v)
-print('FINALLLY: ', fg)
+    print('verticesdict:', verticesdict, 'len', len(verticesdict))
+    print('nonvertices:', nonverticesdict, 'len', len(nonverticesdict))
+    ###################################################
+
+    ################### get all edges in dim
+    edges = list(itertools.product(range(2), repeat=dim))
+    edgesdict = {}
+    for edge in np.array(edges[1:]):
+        edgesdict[' '.join(np.array2string(edge, separator=','))] = edge
+    print('edges:', edgesdict)
+    #############################
+
+    undeterpts={}
+    for vcandob in vcandobs:  #######starting point
+        edgeavail = edgesdict.copy()
+
+        for gernerator in vcandob.dcomp:
+            gerstr = ' '.join(np.array2string(np.array(gernerator), separator=','))
+            if gerstr in edgesdict.keys():
+                edgeavail.pop(gerstr)
+        print('point', vcandob)
+        print('edgeavailable:', edgeavail.values())
+
+        for forward in edgeavail.values():
+            newpoint = np.add(vcandob.vertex, forward)
+            newpointstr = ' '.join(np.array2string(np.array(sorted(newpoint)), separator=','))
+            if newpointstr in verticesdict:
+                continue
+            elif newpointstr in nonverticesdict:
+                continue
+            elif newpointstr in undeterpts:
+                continue
+            elif sum(np.isin(newpoint, (0, k))):
+                continue
+            elif sum(newpoint) > dim*k/2:
+                continue
+            elif cmb(newpoint):
+                nonvertices.append(newpoint)
+                nonverticesdict[newpointstr] = newpoint
+                continue
+            else:
+                undeterpts[newpointstr] = newpoint
+                dcomp = np.append(vcandob.dcomp, [forward], axis=0)     ######add new vertex? back to iteration
+                ptob = sd.Point(newpoint, dcomp)
+                vcandobs.append(ptob)#will we meet a same pt 1+
+                # vcanddict[' '.join(np.array2string(newpoint, separator=','))] = newpoint, dcomp
+    print('undeterpts ', undeterpts.keys())
+    return undeterpts, verticesdict
+########only vertices no generator
+
+def verificationbk():
+    vv = []
+    vv.append(' '.join(np.array2string(np.array([1, 1, 4, 4]), separator=',')))
+    vv.append(' '.join(np.array2string(np.array([1, 2, 2, 5]), separator=',')))
+    vv.append(' '.join(np.array2string(np.array([1, 3, 5, 5] ), separator=',')))
+    vv.append(' '.join(np.array2string(np.array([2, 2, 3, 6]), separator=',')))
+    vv.append(' '.join(np.array2string(np.array([2, 2, 4, 6]), separator=',')))
+    vv.append(' '.join(np.array2string(np.array([1, 2, 4, 5]), separator=',')))
+    fg=True
+    for v in vv:
+        if v not in runheu1()[0].keys():
+            fg=False
+            print('missed: ', v)
+    print('FINALLLY: ', fg)
+
+
+def verificationall():
+    vground = sd.picr(dim, 'perv_d_dcomp')
+    print('verificationall', len(vground[::2]), vground[::2])
+    resultvcand = {**runheu1()[0], **runheu1()[1]}
+    print(len(resultvcand), len(vground[::2]))
+    # print(resultvcand)
+    # print(vground)
+    for ver in vground[::2]:
+        if ' '.join(np.array2string(np.array(ver), separator=',')) not in resultvcand:
+            print(False, ver)
+
+    ####################d=5 only
+    # vground = sd.picr(dim, 'perv')
+    # print('verificationall', len(vground), vground)
+    # resultvcand = {**runheu1()[0], **runheu1()[1]}
+    # print(len(resultvcand), len(vground))
+    # for ver in vground:
+    #     if ' '.join(np.array2string(np.array(ver), separator=',')) not in resultvcand:
+    #         print(False, ver)
+verificationall()
--- gradpro/six003deluxe.py
+++ gradpro/six003deluxe.py
@@ -16,6 +16,8 @@ class Point(object):
     def __str__(self) -> str:
         return str(self.vertex) + ' ' + str(self.dcomp)
 
+    def __repr__(self) -> str:
+        return str(self.vertex) + ' ' + str(self.dcomp)
 
 def funcC1():
     dim = 2
@@ -68,7 +70,7 @@ def funcC1():
         dkvertices = dkverticesq.get()[1]
         pervertices.append(dkvertices)
     # pervertices = np.array(pervertices)
-    np.savetxt("perv d" + str(dim) + ".csv",  pervertices, fmt='%s', delimiter=',', newline='\n')
+    # np.savetxt("perv d" + str(dim) + ".csv",  pervertices, fmt='%s', delimiter=',', newline='\n')
     print('pervertice: ', pervertices)
 
 def funcDcomp(dim):
@@ -77,66 +79,76 @@ def funcDcomp(dim):
     # print('ger: ',gervec)
     gervec.remove((0,)*dim)
     print('ger: ', gervec)
-    pts = []
+    ptsobnct = {}
     ptsob = {}
     for r in range(1, len(gervec)+1):
         comb = itertools.combinations(gervec, r)
         for lists in comb:
             # print('lists: ', lists)
             pt = np.array(lists).sum(axis=0, dtype=int)
-
-########################only keep vertex up to central symmetery
-            # if sum(pt) > dim * k / 2:
-            #     continue
-########################only keep vertex up to central symmetery
-
-            # pt = np.sort(pt)
             ptsstr = ' '.join(np.array2string(pt, separator=','))
             ptob = Point(pt, np.array(lists))
+
+#######################only keep vertex up to central symmetery
+            if sum(pt) > dim * k / 2:
+                # print('pt: ', pt)
+                if ptsstr in ptsobnct:
+                    ptsobnct[ptsstr] = 2
+                else:
+                    ptsobnct[ptsstr] = ptob
+
+#######################only keep vertex up to central symmetery
+            else:
+            # pt = np.sort(pt)
             # print('pt: ', pt)
-            if ptsstr in ptsob:
-                ptsob[ptsstr] = 2
-                continue
-            ptsob[ptsstr] = ptob
+                if ptsstr in ptsob:
+                    ptsob[ptsstr] = 2
+                    ptsobnct[ptsstr] = 2
+                else:
+                    ptsob[ptsstr] = ptob
+                    ptsobnct[ptsstr] = ptob
 
     # print('pts: ', pts)
 
     ptsob = {key: val for key, val in ptsob.items() if val != 2}
+    ptsobnct = {key: val for key, val in ptsobnct.items() if val != 2}
     print('all vertices', ptsob)
     # np.savetxt("all d" + str(dim) + ".csv", pts[hull.vertices], fmt='%s', delimiter=',', newline='\n')
 
+    def reduceruptoperm(ptsob, uptocentral=True):
+    #################reduce up to permutation
+        perverticesset = set()
+        dkverticesq = queue.PriorityQueue()
+        pervertices = {}
+        for key, val in ptsob.items():
+            vertex = val.vertex
+            # print('vertex； ', vertex)
+            vertex = np.sort(vertex)
+            vertexstr = ' '.join(np.array2string(vertex, separator=','))
+            if vertexstr in perverticesset: ########because the smallest permutation is visited first
+                continue
 
-    perverticesset = set()
-    dkverticesq = queue.PriorityQueue()
-    pervertices = {}
-    for key, val in ptsob.items():
-        vertex = val.vertex
-        # print('vertex； ', vertex)
-        vertex = np.sort(vertex)
-        vertexstr = ' '.join(np.array2string(vertex, separator=','))
-        if vertexstr in perverticesset:
-            continue
+    ################# sum x_i = dk/2 minimize the number of coordinates larger than k/2
+            if sum(vertex) == dim*k/2 and uptocentral:
+                perverticesset.add(vertexstr)
+                dkverticesq.put(((vertex > k/2).sum(), key, val))
+                continue
+    ################ sum x_i = dk/2 minimize the number of coordinates larger than k/2
 
-################## sum x_i = dk/2 minimize the number of coordinates larger than k/2
-        # if sum(vertex) == dim*k/2:
-        #     perverticesset.add(vertexstr)
-        #     dkverticesq.put(((vertex > k/2).sum(), key, val))
-        #     continue
-################# sum x_i = dk/2 minimize the number of coordinates larger than k/2
+            perverticesset.add(vertexstr)
+            pervertices[key] = val
+            # print('pervertices', pervertices)
+        if not dkverticesq.empty():
+            dkvertices = dkverticesq.get()[1:]
+            print(dkvertices)
+            pervertices[dkvertices[0]] = dkvertices[1]
+        # pervertices['[ 0 , 0 ]'] = Point(np.zeros(dim, dtype=np.int), ((0,)*dim,))
+        pervertices[' '.join(np.array2string(np.zeros(dim, dtype=np.int), separator=','))] = Point(np.zeros(dim, dtype=np.int), np.array(((0,)*dim,)))
+        # pervertices = np.array(pervertices)
+        # print('pervertice: ', pervertices)
+        return pervertices
 
-        perverticesset.add(vertexstr)
-        pervertices[key] = val
-        # print('pervertices', pervertices)
-    if not dkverticesq.empty():
-        dkvertices = dkverticesq.get()[1:]
-        print(dkvertices)
-        pervertices[dkvertices[0]] = dkvertices[1]
-    # pervertices['[ 0 , 0 ]'] = Point(np.zeros(dim, dtype=np.int), ((0,)*dim,))
-    pervertices[' '.join(np.array2string(np.zeros(dim, dtype=np.int), separator=','))] = Point(np.zeros(dim, dtype=np.int), np.array(((0,)*dim,)))
-    # pervertices = np.array(pervertices)
-    # print('pervertice: ', pervertices)
-
-
+    pervertices = reduceruptoperm(ptsob)
     # arr2save = np.array([[0, 0]], dtype=int)
     arr2save = []
     for key, val in pervertices.items():
@@ -144,10 +156,19 @@ def funcDcomp(dim):
         arr2save.append(val.vertex)
         arr2save.append(val.dcomp)
         # print('arr2save', arr2save)
-    print('arr2save', arr2save)
+    print('arr2save', len(arr2save), arr2save)
     # np.savetxt("perv_d_dcomp" + str(dim) + ".csv",  arr2save, fmt='%s', delimiter=',', newline='\n')
     # np.save("perv_d_dcomp" + str(dim) + ".csv",  arr2save)
-    return arr2save
+
+    ################################save all vertices
+    arr2saveall = []
+    for key, val in reduceruptoperm(ptsobnct, False).items():
+        # print('kv: ', key, val)
+        arr2saveall.append(val.vertex)
+        arr2saveall.append(val.dcomp)
+        # print('arr2save', arr2save)
+    print('arr2saveall', len(arr2saveall), arr2saveall)
+    return arr2save, arr2saveall
 
 def picw(dim, arr2save, filenameprefix='perv_d_dcomp'):
     with open("pickelfile/"+ filenameprefix + str(dim) + ".pkl", 'wb') as fp:
@@ -164,6 +185,31 @@ def picr(dim, filenameprefix='perv_d_dcomp'):
     return itemlist
 
 
-# funcDcomp(2)
-# picw(2, funcDcomp(2), 'perv_dcomp_nct')
+# funcDcomp(3)
+# picw(4, funcDcomp(4), 'perv_dcomp_nct')
+# picw(4, funcDcomp(4, True), 'perv_dcomp')
 # funcC1()
+
+# picw(5, [[0,0,0,0,0],[0,0,0,0,1],[0,0,0,1,2],[0,0,0,2,2],[0,0,1,1,3],[0,0,1,3,3],[0,0,2,2,4],[0,0,2,3,4],[0,0,3,4,4],
+# [0,0,4,4,4],[0,1,1,1,4],[0,1,1,4,4],[0,1,2,2,5],[0,1,2,4,5],[0,1,3,5,5],[0,1,5,5,5],[0,2,2,3,6],[0,2,2,4,6],
+# [0,2,4,6,6],[0,2,5,6,6],[0,3,3,3,7],[0,3,3,5,7],[0,3,4,6,7],[0,3,6,6,7],[0,4,4,4,8],[0,4,4,5,8],[0,4,4,7,7],
+# [0,4,5,6,8],[0,4,6,6,8],[0,4,7,7,7],[0,5,5,7,8],[0,5,7,7,8],[0,6,6,8,8],[0,6,7,8,8],[0,7,8,8,8],[0,8,8,8,8],
+# [1,1,1,1,5],[1,1,1,5,5],[1,1,2,2,6],[1,1,2,5,6],[1,1,3,6,6],[1,1,6,6,6],[1,2,2,3,7],[1,2,2,5,7],[1,2,4,7,7],
+# [1,2,6,7,7],[1,3,3,3,8],[1,3,3,6,8],[1,3,4,7,8],[1,3,7,7,8],[1,4,4,4,9],[1,4,4,6,9],[1,4,4,8,8],[1,4,5,7,9],
+# [1,4,7,7,9],[1,4,8,8,8],[1,5,5,8,9],[1,5,8,8,9],[1,6,6,9,9],[1,6,8,9,9],[1,7,9,9,9],[1,9,9,9,9],[2,2,2,4,8],
+# [2,2,2,5,8],[2,2,5,8,8],[2,2,6,8,8],[2,3,3,4,9],[2,3,3,6,9],[2,3,5,8,9],[2,3,7,8,9],[2,4,4,5,10],[2,4,4,6,10],
+# [2,4,5,9,9],[2,4,6,8,10],[2,4,7,8,10],[2,4,8,9,9],[2,5,6,9,10],[2,5,8,9,10],[2,6,7,10,10],[2,6,8,10,10],
+# [2,8,10,10,10],[3,3,4,4,10],[3,3,4,7,10],[3,3,5,8,10],[3,3,8,8,10],[3,4,5,5,11],[3,4,5,7,11],
+# [3,4,6,8,11],[3,4,8,8,11],[3,4,9,9,10],[3,5,5,10,10],[3,5,9,9,11],[3,6,6,10,11],[3,7,7,11,11],
+# [4,4,4,4,11],[4,4,4,8,11],[4,4,5,9,11],[4,4,6,6,12],[4,4,6,7,12],[4,4,7,8,12],[4,4,8,8,12],
+# [4,4,10,10,10],[4,5,5,5,12],[4,5,5,8,12],[4,5,5,10,11],[4,5,6,9,12],[4,6,6,10,12],[5,5,5,11,11],
+# [5,5,6,6,13],[5,5,6,8,13],[5,5,7,9,13],[6,6,6,7,14]], 'perv')
+
+# twodvertices = funcDcomp(5)
+# picw(5, twodvertices[1], 'testperv_dcomp_nct')
+# picw(5, twodvertices[0], 'testperv_dcomp')
+
+# leqflag=False
+# print(3 > 4 and leqflag or 3 < 4 and not leqflag )
+# res = picr(3)
+# print(picr(3))
